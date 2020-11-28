00ada7cccfaa56b9204ef27fa13c89b107ec7565
Ilya Yermolovich <ilya1099@mail.ru>
2020-11-28 12:14:22 +0300

    Refactor Lab4.2, add Gauss filter

--- Lab4.2/main.py
+++ Lab4.2/main.py
@@ -17,25 +17,36 @@ clarity = np.array([
         [[-1], [-1], [-1]]
 ])
 
-def convolution_matrix_antialiasing(m, depth):
-    mul = np.multiply(m, convolution)
+gauss = np.array([
+        [[0.05472157], [0.11098164], [0.05472157]],
+        [[0.11098164], [0.22508352], [0.11098164]],
+        [[0.05472157], [0.11098164], [0.05472157]]
+])
+
+
+def matrix_antialiasing(m, filter, dv):
+    mul = np.multiply(m, filter)
     lev1 = np.sum(mul, axis=0)
     lev2 = np.sum(lev1, axis=0)
     ans = []
-    dv = 1 / 6
     for el in lev2:
-        ans.append(int(el * dv))
+        nel = int(el * dv)
+        nel = nel if nel <= 255 else 255
+        nel = nel if nel >= 0 else 0
+        ans.append(nel)
     return ans
+
+
+def convolution_matrix_antialiasing(m, depth):
+    return matrix_antialiasing(m, convolution, 1 / 6)
+
+
+def gauss_antialiasing(m, depth):
+    return matrix_antialiasing(m, gauss, 1)
 
 
 def improving_clarity_antialiasing(m, depth):
-    mul = np.multiply(m, clarity)
-    lev1 = np.sum(mul, axis=0)
-    lev2 = np.sum(lev1, axis=0)
-    ans = []
-    for el in lev2:
-        ans.append(int(el))
-    return ans
+    return matrix_antialiasing(m, clarity, 1)
 
 
 def default(m, depth):
@@ -61,6 +72,9 @@ def filter_algorithm(m, window, func):
     if not (window % 2):
         raise Exception("Sorry, even number")
 
+    if func.__name__ != "averaged_antialiasing":
+        window = 3
+
     answer = m.tolist()
     rows, cols, colors = m.shape
     hw = window // 2
@@ -79,14 +93,14 @@ LABELS = {
     'Без редактирования': default,
     'Медианный фильтр': averaged_antialiasing,
     'Матрица свертки': convolution_matrix_antialiasing,
-    'Улучшение четкости': improving_clarity_antialiasing
+    'Улучшение четкости': improving_clarity_antialiasing,
+    'Размытие по Гаусс': gauss_antialiasing
 }
 
 def main():
 
     fig = plt.figure(figsize=(10, 4))
     img = plt.imread('website.jpg')
-    rows, cols, colors = img.shape
 
     axload = plt.axes([0.05, 0.8, 0.25, 0.075])
     bload = Button(axload, 'Открыть изображение')
@@ -95,7 +109,7 @@ def main():
     slider = Slider(axes, '${}$'.format('Ядро'), 1, 17, valinit=3, valfmt=r'$%d$', valstep=2)
 
     rax = plt.axes([0.05, 0.4, 0.25, 0.25])
-    radio = RadioButtons(rax, ('Без редактирования', 'Медианный фильтр', 'Матрица свертки', 'Улучшение четкости'), active=0)
+    radio = RadioButtons(rax, LABELS.keys(), active=0)
 
     plt.axes([0.27, 0.1, 0.8, 0.8])
     plt.xticks([])
@@ -103,7 +117,7 @@ def main():
     image = plt.imshow(img)
 
     def load(val):
-        filename = filedialog.askopenfilename(initialdir="/", title="Select A File")
+        filename = filedialog.askopenfilename(initialdir="/", title="Image", filetypes=[("IMG", "*.jpg;*jpeg;*.png")])
         img = plt.imread(filename)
         image.set_data(img)
         image.axes.figure.canvas.draw()
